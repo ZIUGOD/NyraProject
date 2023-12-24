3e3f010296efa4aa651909a1a881bdaea13ff204
ZIUGOD <caique.dubstep@gmail.com>
2023-12-24 17:12:14 -0300

    Fix templates directory

--- Nyra/core/urls.py
+++ Nyra/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from notes.views import NoteListView
 
 urlpatterns = [
+    path("", NoteListView.as_view(), name="home_page"),
     path("admin/", admin.site.urls),
-    path("note/", include("notes.urls")),  # generic route
+    path("note/", include("notes.urls")),
 ]
